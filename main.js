9f70272666e44bbb80b16a8295d1a2829effe058
ezequieltech <ezequiel.tech@escola.pr.gov.br>
2026-04-21 16:52:24 -0300

    r

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ for (let i = 0; i < botoes.length; i++) {
 } 
 }
 
-const contadores = document.querySelectorAll(".contadores");
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
 
 contadores[0].textContent = tempoObjetivo1;
