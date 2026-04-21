9173ad35f256aa70c22f9d7527ef5b532d85f580
ezequieltech <ezequiel.tech@escola.pr.gov.br>
2026-04-21 17:12:58 -0300

    d

--- main.js
+++ main.js
@@ -25,6 +25,8 @@ let tempoFinal = tempoObjetivo - tempoAtual;
 let segundos = Math.floor( tempoFinal/1000);
 let minutos = Math.floor(segundos/60);
 let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
 
-return horas; 
+
+return dias; 
 }
